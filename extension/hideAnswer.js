63ab976d8ceba8862b41554532e40b7e8fb5418e
AbdeI1 <abdelrahmanmokbel@gmail.com>
2022-06-25 00:53:16 -0500

    changes

---
+++ extension/hideAnswer.js
@@ -0,0 +1,19 @@
+var ansElement = document.getElementsByClassName("strong")[0]
+
+if(ansElement) {
+  const ans = ansElement.innerHTML
+  ansElement.innerHTML = "REDACTED"
+  ansElement.style.color = "red"
+  let button = document.createElement("button")
+  button.innerHTML = "Show Answer"
+  button.type = "button"
+  function test() {
+    console.log("testing")
+  }
+  button.addEventListener("click", function() { 
+    ansElement.style.color = ""
+    ansElement.innerHTML = ans
+    button.remove()
+  })
+  ansElement.parentElement.after(button)
+}
